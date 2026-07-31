62a9ae59b9145c2fee45fa4eacf1b16c275ebf54
wc402 <wc402@cam.ac.uk>
2026-07-31 12:59:44 +0100

    Added Imm generator, CU, ALU and ALU control

--- riscVprocessor_attempt1.srcs/sources_1/new/riscV.v
+++ riscVprocessor_attempt1.srcs/sources_1/new/riscV.v
@@ -57,9 +57,9 @@ endmodule
 
 // Register File - file of registers that holds values currently being used
 
-module register_file (clk, reset, rs1, rs2, rd, write_data, read_data1, read_data2, write);
+module register_file (clk, reset, rs1, rs2, rd, write_data, read_data1, read_data2, regwrite);
 
-input clk, reset, write;
+input clk, reset, regwrite;
 input [4:0] rs1, rs2, rd;
 input [31:0] write_data;
 output [31:0] read_data1, read_data2;
@@ -74,7 +74,7 @@ if(reset) begin
     for(k=0; k<32; k=k+1) begin
     Registers[k] <= 32'b00;
     end
-end else if(write) begin
+end else if(regwrite) begin
     Registers[rd] <= write_data;
     end 
 end
@@ -84,10 +84,97 @@ assign read_data2 = Registers[rs2];
 
 endmodule
 
-// Immediate Generator
+// Immediate Generator - for immediate instruction (specify a number rather than variable)
 module ImmGen(Opcode, instruction, ImmExt);
 
+input [6:0] Opcode;
+input [31:0] instruction;
+output reg [31:0] ImmExt;
+
+always @(*) begin
+// case examines each OpCode to determine whether it is a R-format, lw, sw, or beq instruction
+    case(Opcode)
+    7'b0000011 : ImmExt = {{20{instruction[31]}}, instruction[31:20]};
+    // Load type = 20 copies of MSB and concatinates with immediate value from bits 31 to 20
+    7'b0100011 : ImmExt = {{20{instruction[31]}}, instruction[31:25], instruction[11:7]};
+    // Store type
+    7'b1100011 : ImmExt = {{19{instruction[31]}}, instruction[31], instruction[30:25], instruction[11:8],1'b0};
+    // Branch type
+    endcase
+
+end    
 endmodule
+
+// Control Unit gives outputs determining behaviour of different components of our processor
+module control_unit(instruction, branch, memread, memtoreg, ALUop, memwrite, ALUsrc, regwrite);
+
+input [6:0] instruction;
+output reg branch, memread, memtoreg, memwrite, ALUsrc, regwrite;
+output reg [1:0] ALUop;
+
+always @(*) begin
+    case(instruction)
+    7'b0110011 : {ALUsrc, memtoreg, regwrite, memread, memwrite, branch, ALUop} <= 8'b001000_10;
+    // R-format (instructions involving operation performed on values in both rs1 and rs2) rather than Imm
+    7'b0000011 : {ALUsrc, memtoreg, regwrite, memread, memwrite, branch, ALUop} <= 8'b111100_00;
+    // Load type
+    7'b0100011 : {ALUsrc, memtoreg, regwrite, memread, memwrite, branch, ALUop} <= 8'b100010_00;
+    // Store type
+    7'b1100011 : {ALUsrc, memtoreg, regwrite, memread, memwrite, branch, ALUop} <= 8'b000001_01;
+    // Immediate type
+    endcase
+end
+endmodule
+
+
+// ALU module where the operations actually take place, add sub OR AND etc.
+module ALU(A, B, Control_in, ALU_result, zero);
+
+input [31:0] A, B;
+input [3:0] Control_in;
+output reg zero;
+output reg [31:0] ALU_result;
+
+always @(Control_in or A or B) begin
+    case(Control_in)
+    4'b0000 : begin zero <= 0; 
+                    ALU_result <= A&B; end
+    4'b0001 : begin zero <= 0;
+                    ALU_result <= A | B; end                    
+    4'b0010 : begin zero <= 0; 
+                    ALU_result <= A + B; end
+    4'b0110 : begin if(A==B) 
+                        zero <= 1; 
+                    else 
+                        zero <= 0; ALU_result <= A - B; end
+    endcase
+end                 
+endmodule
+
+// ALU control determines the function that the ALU will carry out depending on instruction
+module ALU_control (ALUop, fun7, fun3, Control_out);
+
+input fun7;
+input [2:0] fun3;
+input [1:0] ALUop;
+output reg [3:0] Control_out;
+
+always @(*) begin
+    case({ALUop, fun7, fun3})
+    6'b00_0_000 : Control_out <= 4'b0010;
+    6'b01_0_000 : Control_out <= 4'b0110;
+    6'b10_0_000 : Control_out <= 4'b0010;
+    6'b10_0_000 : Control_out <= 4'b0110;
+    6'b10_0_111 : Control_out <= 4'b0000;
+    6'b10_0_110 : Control_out <= 4'b0001;
+    endcase
+
+end
+endmodule 
+
+
+
+
 
 
 
